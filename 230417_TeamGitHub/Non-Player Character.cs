46424ea969d5ef27b573464b12c7632b3e960203
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-17 21:24:09 +0900

    feature: NPC
    아직 완료안됬습니다

---
+++ 230417_TeamGitHub/Non-Player Character.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace _230417_TeamGitHub
+{
+    public class Non_Player_Character
+    {
+        private string name;
+        public Non_Player_Character(string name)
+        {
+            this.name = name;
+        }
+    }
+    public class OldNpc : Non_Player_Character
+    {
+        public OldNpc(string name) : base(name)
+        {
+            string str = "허허허....";
+            string str1 = "아이구...허리야...";
+            string str2 = "누가 이 늙은이좀 도와줬으면 하는데...";
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+        }
+
+    }
+    public class GirlNpc : Non_Player_Character
+    {
+        public GirlNpc(string name) : base(name)
+        {
+            string str = "꺄르륵!!";
+            string str1 = "못보던 아저씨다~";
+            string str2 = "아저씨는 어디서 왔어요?";
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+        }
+
+    }
+    public class BoyNpc : Non_Player_Character
+    {
+        public BoyNpc(string name) : base(name)
+        {
+            string Boy = "거기서!";
+            string Boy1 = "";
+            string Boy2 = "";
+            Console.WriteLine(Boy);
+            Console.WriteLine(Boy1);
+            Console.WriteLine(Boy2);
+        }
+
+    }
+    public class MissNpc : Non_Player_Character
+    {
+        public MissNpc(string name) : base(name)
+        {
+            string str = "허허허....";
+            string str1 = "아이구...허리야...";
+            string str2 = "누가 이 늙은이좀 도와줬으면 하는데...";
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+        }
+
+    }
+    public class YouthNpc : Non_Player_Character
+    {
+        public YouthNpc(string name) : base(name)
+        {
+            string str = "허허허....";
+            string str1 = "아이구...허리야...";
+            string str2 = "누가 이 늙은이좀 도와줬으면 하는데...";
+            Console.WriteLine(str);
+            Console.WriteLine(str1);
+            Console.WriteLine(str2);
+        }
+
+    }
+}
