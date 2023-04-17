2dd03b3715f84f2f433dee31ae041357a6f4da56
Pass1948 <129037485+Pass1948@users.noreply.github.com>
2023-04-17 20:19:27 +0900

    feat : 스테이지(얼음섬) 제작 시작

---
+++ 230417_TeamGitHub/Stage2_IceLand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230417_TeamGitHub
+{
+    public class Stage2_IceLand
+    {
+        // 배경 이미지 적용
+        // 방 크기
+    }
+
+    public class IceLand_Room1 : Stage1_Forest
+    {
+    }
+
+    public class IceLand_Room2 : Stage1_Forest
+    {
+    }
+
+    public class IceLand_Room3 : Stage1_Forest
+    {
+    }
+
+    public class IceLand_BossRoom : Stage1_Forest
+    {
+    }
+
+}
