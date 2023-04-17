bca5d6d666873127029beca9ba6521ecfbfffaa8
Cyanligth <129037620+Cyanligth@users.noreply.github.com>
2023-04-17 21:27:32 +0900

    feat : 몬스터 추가
    일반 몹 3종, 엘리트몹 3종, 보스몹 1종 추가

--- 230417_TeamGitHub/Monster.cs
+++ 230417_TeamGitHub/Monster.cs
@@ -42,4 +42,18 @@ namespace _230417_TeamGitHub
         // 좀비같이 생긴 좀비
     }
 
+    public class GoldenGoblin : Goblin
+    {
+        // 황금 고블린이다. 죽이면 좋은 템을 드랍한다.
+    }
+
+    public class Bat : Monster
+    {
+        // 흡혈박쥐다. 체력은 허접하지만 공격할때마다 피를 빨아먹으니 한방에 처리하도록 하자.
+    }
+
+    public class lesserDemon : Monster
+    {
+        // 레서 데몬. 비교적 튼튼하고 강하다. 하지만 특별한 능력은 없다.
+    }
 }
