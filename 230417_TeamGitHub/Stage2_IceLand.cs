56bd3479160a9dbdc503071a2eab5ea56fa56000
Pass1948 <129037485+Pass1948@users.noreply.github.com>
2023-04-17 20:42:18 +0900

    feat : 스테이지2(얼음섬) 점프방 구현
    점프발판 구현 및 부모 클래스 수정

--- 230417_TeamGitHub/Stage2_IceLand.cs
+++ 230417_TeamGitHub/Stage2_IceLand.cs
@@ -18,7 +18,7 @@ namespace _230417_TeamGitHub
         // 기믹 구조물 구현(함정, 퍼즐)_(추상)
     }
 
-    public class IceLand_BasicRoom1 : Stage1_Forest
+    public class IceLand_BasicRoom1 : Stage2_IceLand
     {
         // 배경 이미지 적용
         // 방 크기
@@ -28,7 +28,7 @@ namespace _230417_TeamGitHub
         // 기믹 구조물 구현(함정, 퍼즐)_(추상)
     }
 
-    public class IceLand_BuildingRoom2 : Stage1_Forest
+    public class IceLand_BuildingRoom2 : Stage2_IceLand
     {
         // 배경 이미지 적용
         // 방 크기
@@ -38,12 +38,21 @@ namespace _230417_TeamGitHub
         // 얼음 미끌거림 기능 추가
     }
 
-    public class IceLand_JumpRoom3 : Stage1_Forest
+    public class IceLand_JumpRoom3 : Stage2_IceLand
     {
+        // 배경 이미지 적용
+        // 방 크기
+        // 얼음땅 구현(부서지는 땅 및 괜찮은땅)
+        // 얼음 미끌거림 기능
+        // 얼음 관련 구조물 구현(배경 및 파괴 상호작용)_(추상)
+        // 기믹 구조물 구현(함정, 퍼즐)_(추상)
+        // 점프발탄 추가
+
     }
 
-    public class IceLand_BossRoom : Stage1_Forest
+    public class IceLand_BossRoom : Stage2_IceLand
     {
+
     }
 
 }
