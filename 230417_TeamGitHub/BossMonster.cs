30487f9be85d943eae734fab1214455db51944ed
Cyanligth <129037620+Cyanligth@users.noreply.github.com>
2023-04-17 20:49:30 +0900

    feat : 보스몬스터 1종 추가
    자이언트 골렘. 겁나 튼튼하다.

--- 230417_TeamGitHub/BossMonster.cs
+++ 230417_TeamGitHub/BossMonster.cs
@@ -14,4 +14,15 @@ namespace _230417_TeamGitHub
             // 일반몹은 없는 특별한 패시브 스킬. 보스는 만만하지 않다!
         }
     }
+    public class GiganticGolem : BossMonster
+    {
+        public override void Skill()
+        {
+            // 스타 라이트 브레이커-!....가 아니라 그냥 굉장한 빔. 맞으면 녹아버릴지도?
+        }
+        public override void Passive()
+        {
+            // 이것은 골렘의 외피입니다. 끔찍하게 튼튼하죠. 물리공격에 매우 높은 내성을 지닙니다.
+        }
+    }
 }
