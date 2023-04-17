577a38c1fbe7411eaf3b2580e165be633834e5a9
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-17 21:43:15 +0900

    feat : NPC 추가
    NPC 여럿 추가중..

--- 230417_TeamGitHub/Non-Player Character.cs
+++ 230417_TeamGitHub/Non-Player Character.cs
@@ -46,34 +46,21 @@ namespace _230417_TeamGitHub
         public BoyNpc(string name) : base(name)
         {
             string Boy = "거기서!";
-            string Boy1 = "";
-            string Boy2 = "";
+            string Boy1 = "잡히기만 해봐!";
+            string Boy2 = "저 아저씨는 누구지?";
             Console.WriteLine(Boy);
             Console.WriteLine(Boy1);
             Console.WriteLine(Boy2);
         }
 
     }
-    public class MissNpc : Non_Player_Character
+    public class WomanNpc : Non_Player_Character
     {
-        public MissNpc(string name) : base(name)
+        public WomanNpc(string name) : base(name)
         {
-            string str = "허허허....";
-            string str1 = "아이구...허리야...";
-            string str2 = "누가 이 늙은이좀 도와줬으면 하는데...";
-            Console.WriteLine(str);
-            Console.WriteLine(str1);
-            Console.WriteLine(str2);
-        }
-
-    }
-    public class YouthNpc : Non_Player_Character
-    {
-        public YouthNpc(string name) : base(name)
-        {
-            string str = "허허허....";
-            string str1 = "아이구...허리야...";
-            string str2 = "누가 이 늙은이좀 도와줬으면 하는데...";
+            string str = "어디서 온 청년이지?";
+            string str1 = "우리 마을은 무슨일로 왔어요?";
+            string str2 = "이놈의 남편은 어디있는거야!?";
             Console.WriteLine(str);
             Console.WriteLine(str1);
             Console.WriteLine(str2);
