2be1fc3ea07e4153a72f97157776a03c09036079
현수 조 <jhsroid@naver.com>
2023-04-17 21:09:51 +0900

    feat : 창 선언 함수 추가

--- 230417_TeamGitHub/Spear.cs
+++ 230417_TeamGitHub/Spear.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace _230417_TeamGitHub
 {
-    internal class Spear
+    internal class Spear : Weapon
     {
+        public Spear(string name, int attackPoint) : base(name, attackPoint)
+        {
+            // 사정거리 매우매우 긺
+            // 공격속도 보통
+            // 공격력 조금 높음
+        }
     }
 }
