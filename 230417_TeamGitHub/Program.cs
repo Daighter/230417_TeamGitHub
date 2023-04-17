c0fe897a4533eb3b043d7e9fb6cea6acf4f2ed33
현수 조 <jhsroid@naver.com>
2023-04-17 19:27:29 +0900

    초기 커밋.

---
+++ 230417_TeamGitHub/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _230417_TeamGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
