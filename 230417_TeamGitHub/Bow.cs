a5d88fe3f60755a08b58ad13f98ac0faf64adf95
현수 조 <jhsroid@naver.com>
2023-04-17 21:12:22 +0900

    feat : 활 선언 함수 추가

--- 230417_TeamGitHub/Bow.cs
+++ 230417_TeamGitHub/Bow.cs
@@ -8,5 +8,11 @@ namespace _230417_TeamGitHub
 {
     internal class Bow : Weapon
     {
+        public Bow(string name, int attackPoint) : base(name, attackPoint)
+        {
+            // 사정거리 개인차 있음
+            // 공격속도 조금 빠름
+            // 공격력 화살에 따라 다름
+        }
     }
 }
