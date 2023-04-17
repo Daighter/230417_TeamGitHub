f422643fa307b93cc4e2e740fcf0e98b4487c065
Pass1948 <129037485+Pass1948@users.noreply.github.com>
2023-04-17 20:13:28 +0900

    feat : 스테이지1(숲)_Room3
    점프맵 구현

--- 230417_TeamGitHub/Stage1_Foest.cs
+++ 230417_TeamGitHub/Stage1_Foest.cs
@@ -34,4 +34,13 @@ namespace _230417_TeamGitHub
         // 상점, 강화, 스킬 등 건물 위치
     }
 
+    public class Forest_Room3 : Stage1_Forest
+    {
+        // 배경 이미지 적용
+        // 방 크기
+        // 풀 관련 구조물 구현(배경 및 파괴 상호작용)
+        // 기믹 구조물 구현(함정, 퍼즐)
+        // 점프맵 구현
+    }
+
 }
