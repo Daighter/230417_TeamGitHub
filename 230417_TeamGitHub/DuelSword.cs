7dd796166372ff102bfeead32d74b7abdb2bea9c
현수 조 <jhsroid@naver.com>
2023-04-17 20:52:28 +0900

    feat : 쌍검호출 함수 추가

--- 230417_TeamGitHub/DuelSword.cs
+++ 230417_TeamGitHub/DuelSword.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace _230417_TeamGitHub
 {
-    internal class DuelSword
+    internal class DuelSword : Weapon
     {
+        public DuelSword(string name, int attackPoint) : base(name, attackPoint)
+        {
+            // 사정거리 매우짧음
+            // 공격력 매우 낮음
+            // 공격속도 매우 빠름
+            // 착용자에게 민칩함 추가부여
+        }
     }
 }
