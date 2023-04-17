45db08f09cbb3b2dfa47dece998435e4df759078
Cyanligth <129037620+Cyanligth@users.noreply.github.com>
2023-04-17 19:50:03 +0900

    feat : 몬스터 스텟 추가
    몬스터 id, 이름, 체력, 공격력 정의

---
+++ 230417_TeamGitHub/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230417_TeamGitHub
+{
+    public class Monster
+    {
+        protected int id;
+        protected string name;
+        protected int hp;
+        protected int atk;
+    }
+}
