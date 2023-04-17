bc08caf868116bf626de8d827a7505ba666febac
현수 조 <jhsroid@naver.com>
2023-04-17 20:43:52 +0900

    feat : 롱소드의 특성 추가

--- 230417_TeamGitHub/LongSword.cs
+++ 230417_TeamGitHub/LongSword.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace _230417_TeamGitHub
 {
-    internal class LongSword
+    internal class LongSword : Weapon
     {
+        public LongSword(string name, int attackPoint) : base(name, attackPoint)
+        {
+            // 사정거리가 마우 긺
+            // 공격력이 짱셈
+            // 공격속도가 느림
+        }
     }
 }
