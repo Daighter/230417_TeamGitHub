6c8d5849a8a824e3283aad7a3fbcef6ec19be67b
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-17 21:24:34 +0900

    테스트

--- 230417_TeamGitHub/Program.cs
+++ 230417_TeamGitHub/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Player player = new Player( "용사", 100, 20, 10, 0);
+
+            player.Attack(Player);
+            player.TrueDamage(10);
+            player.MagicAttack(10);
         }
     }
 }
