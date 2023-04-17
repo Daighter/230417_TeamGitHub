501cfe525498c978251e0e22607a2eb27db18470
현수 조 <jhsroid@naver.com>
2023-04-17 21:15:53 +0900

    feat : 릴리즈 파일버스터 선언부 추가

--- 230417_TeamGitHub/ReleasePileBuster.cs
+++ 230417_TeamGitHub/ReleasePileBuster.cs
@@ -8,6 +8,11 @@ namespace _230417_TeamGitHub
 {
     internal class ReleasePileBuster : Weapon
     {
-        
+        public ReleasePileBuster(string name, int attackPoint) : base(name, attackPoint)
+        {
+            // 사정거리 최장
+            // 공격력 최강
+            // 공격속도 최하
+        }
     }
 }
