7a41ee0568875264f31e45677a6f1462245b701e
Cyanligth <129037620+Cyanligth@users.noreply.github.com>
2023-04-17 20:48:47 +0900

    feat : 보스몬스터 개념 추가
    보스몬스터 만드는 기능 추가

---
+++ 230417_TeamGitHub/BossMonster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _230417_TeamGitHub
+{
+    public class BossMonster : EliteMonster
+    {
+        // 굉장히 굉장하게 굉장한 보스몬스터. 엘리트 몹보다 훨씬 강하다.
+        public virtual void Passive()
+        {
+            // 일반몹은 없는 특별한 패시브 스킬. 보스는 만만하지 않다!
+        }
+    }
+}
